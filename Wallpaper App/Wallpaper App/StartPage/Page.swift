1e72b01ccd6e5eb4b652188cd6c929486d352a36
Christina Stadnytska <3suslik@gmail.com>
2019-09-26 13:49:02 +0300

    Add new Project

---
+++ Wallpaper App/Wallpaper App/StartPage/Page.swift
@@ -0,0 +1,14 @@
+//
+//  Page.swift
+//  Wallpaper App
+//
+//  Created by XP on 9/16/19.
+//  Copyright © 2019 XP. All rights reserved.
+//
+
+import UIKit
+
+struct Page {
+    var image = UIImage()
+    var text = String()
+}
