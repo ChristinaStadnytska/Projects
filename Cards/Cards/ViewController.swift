2971a4a294ca8f4161575df94ecb0aaaab15327a
ChristinaStadnytska <kstadnytska@softjourn.com>
2020-01-22 10:40:38 +0200

    drawing text over Core Graphics

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -16,13 +16,27 @@ class ViewController: UIViewController {
     }
 
     func animate() {
-        let path = UIBezierPath()
+//        let path = UIBezierPath()
         let origin = CGPoint(x: self.view.frame.origin.x, y: self.view.frame.origin.y + self.view.bounds.height / 3)
         let size = CGSize(width: self.view.bounds.width, height: self.view.frame.height / 8)
         let containerView = UIView(frame: CGRect(origin: origin, size: size))
         containerView.backgroundColor = #colorLiteral(red: 0.05882352963, green: 0.180392161, blue: 0.2470588237, alpha: 1)
         
         self.view.addSubview(containerView)
+        
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: containerView.bounds.width, height: containerView.bounds.height))
+        let img = renderer.image{ ctx in
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let attrs = [NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Thin", size: 36)!, NSAttributedString.Key.paragraphStyle: paragraphStyle]
+            let string = "Hello"
+            string.draw(with: CGRect(x: 32, y: 32, width: 448, height: 448), options: .usesLineFragmentOrigin, attributes: attrs, context: nil)
+        }
+        
+        let imageView = UIImageView(image: img)
+        containerView.addSubview(imageView)
+        
         
 //        path.move(to: CGPoint(x: containerView.frame.maxX, y: containerView.frame.maxY))
 //        path.addQuadCurve(to: CGPoint(x: 0, y: containerView.frame.maxY), controlPoint: CGPoint(x: containerView.frame.maxX/2, y: 0))
