fb4cd6dd1f514af33c4528881fffaa00ad397f44
ChristinaStadnytska <kstadnytska@softjourn.com>
2020-01-23 11:14:59 +0200

    start cPath

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -9,14 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let cPath = UIBezierPath()
+    let aPath = UIBezierPath()
+    let rPath = UIBezierPath()
+    let dPath = UIBezierPath()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        animate()
+        
+        makeLogo()
+        addAnimationPath()
     }
 
-    func animate() {
-//        let path = UIBezierPath()
+    func addAnimationPath() {
+        // Make background container
         let origin = CGPoint(x: self.view.frame.origin.x, y: self.view.frame.origin.y + self.view.bounds.height / 3)
         let size = CGSize(width: self.view.bounds.width, height: self.view.frame.height / 8)
         let containerView = UIView(frame: CGRect(origin: origin, size: size))
@@ -24,33 +31,46 @@ class ViewController: UIViewController {
         
         self.view.addSubview(containerView)
         
-        let renderer = UIGraphicsImageRenderer(size: CGSize(width: containerView.bounds.width, height: containerView.bounds.height))
-        let img = renderer.image{ ctx in
-            let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.alignment = .center
-            
-            let attrs = [NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Thin", size: 36)!, NSAttributedString.Key.paragraphStyle: paragraphStyle]
-            let string = "Hello"
-            string.draw(with: CGRect(x: 32, y: 32, width: 448, height: 448), options: .usesLineFragmentOrigin, attributes: attrs, context: nil)
-        }
+        // Draw logo
+        UIColor.white.setFill()
+        cPath.fill()
         
-        let imageView = UIImageView(image: img)
-        containerView.addSubview(imageView)
+        UIColor.purple.setStroke()
+        cPath.lineWidth = 2
+        cPath.stroke()
         
+        let layer = CAShapeLayer()
+        layer.path = cPath.cgPath
         
-//        path.move(to: CGPoint(x: containerView.frame.maxX, y: containerView.frame.maxY))
-//        path.addQuadCurve(to: CGPoint(x: 0, y: containerView.frame.maxY), controlPoint: CGPoint(x: containerView.frame.maxX/2, y: 0))
-//
-//        let animation = CAKeyframeAnimation(keyPath: "position")
-//        animation.path = path.cgPath
-//
-//        animation.repeatCount = 0
-//        animation.duration = 2.0
-//
-//        containerView.layer.add(animation, forKey: "animate")
-//        containerView.center = CGPoint(x: 0, y: containerView.frame.maxY)
+        layer.strokeEnd = 0
+        layer.lineWidth = 2
+        layer.borderColor = UIColor.red.cgColor
+        layer.shadowColor = UIColor.yellow.cgColor
+        layer.shadowRadius = 12
+        layer.shadowOpacity = 0.8
+        layer.shadowOffset = CGSize(width: 1, height: 1)
+        layer.strokeColor = UIColor.blue.cgColor
+        layer.fillColor = UIColor.white.cgColor
+        
+        containerView.layer.addSublayer(layer)
+        
+        // Add animation
     }
+    
+    func makeLogo() {
 
+        // cPath
+        cPath.move(to: CGPoint(x: 50, y: 30))
+        cPath.addCurve(to: CGPoint(x: 50, y: 80), controlPoint1: CGPoint(x: 30, y: 20), controlPoint2: CGPoint(x: 0, y: 50))
+//        path.addCurve(to: CGPoint(x: 74.1, y: 49.26), controlPoint1: CGPoint(x: 84.23, y: 46.96), controlPoint2: CGPoint(x: 79.37, y: 49.69))
+//        path.addCurve(to: CGPoint(x: 74, y: 47.52), controlPoint1: CGPoint(x: 74.03, y: 48.71), controlPoint2: CGPoint(x: 74, y: 48.13))
+//        path.addCurve(to: CGPoint(x: 79.3, y: 34.51), controlPoint1: CGPoint(x: 74, y: 43.1), controlPoint2: CGPoint(x: 75.91, y: 38.38))
+//        path.addCurve(to: CGPoint(x: 85.76, y: 29.63), controlPoint1: CGPoint(x: 80.99, y: 32.55), controlPoint2: CGPoint(x: 83.15, y: 30.93))
+//        path.addCurve(to: CGPoint(x: 93.15, y: 27.52), controlPoint1: CGPoint(x: 88.37, y: 28.35), controlPoint2: CGPoint(x: 90.83, y: 27.65))
+//        path.addCurve(to: CGPoint(x: 93.25, y: 29.36), controlPoint1: CGPoint(x: 93.22, y: 28.14), controlPoint2: CGPoint(x: 93.25, y: 28.75))
+//        path.addLine(to: CGPoint(x: 93.25, y: 29.36))
 
+        cPath.close()
+    }
 }
 
